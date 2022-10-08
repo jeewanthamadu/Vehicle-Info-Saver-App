d8ee5a90705aa0675dc65abb540c24b67eb332dd
jeewanthamadu <jeewanthamadu2000@gmail.com>
2022-10-08 14:16:18 +0530

    update models

---
+++ backEnd/model/car.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+
+const carSchema = new mongoose.Schema({
+    vehicleBrandName:{
+        type:String,
+        required:true
+    },
+    vehicleRegNumber:{
+        type:Date,
+        required: true
+    },
+    FuelType:{
+        type:String,
+        required:true
+    },
+   description:{
+        type:String,
+        required:true
+        
+    }
+   
+})
+
+module.exports = mongoose.model('car',carSchema)
